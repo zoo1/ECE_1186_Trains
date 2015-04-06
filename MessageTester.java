2ad2f76afdf6b7eb0f31ad255fad65e9ade271d3
Justin Ying <just10ying@gmail.com>
2015-04-05 20:35:41 -0400

    Time Modifier
    Fixed: time modifier can now never be zero.  At minimum, it is 0.001.

---
+++ MessageTester.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class MessageTester {
+	
+	public static void main(String args[]) {
+		String message;
+		while (true) {
+			System.out.println("Enter a message: ");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				message = br.readLine();
+				MessageLibrary.sendMessage(8001, message);
+			}
+			catch (Exception e) {}				
+		}
+	}
+	
+}
