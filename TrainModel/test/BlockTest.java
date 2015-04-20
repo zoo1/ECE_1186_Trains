82df95f0f6a2d1475df48a64d5df665a61a21acd
Zachary Oscar Ovington <zoo1@pitt.edu>
2015-04-19 20:57:53 -0400

    Block Unit Tests

--- TrainModel/test/BlockTest.java
+++ TrainModel/test/BlockTest.java
@@ -37,140 +37,178 @@ public class BlockTest {
     }
 
     /**
-     * Test of createblock method, of class Block.
+     * Test of if gradient is too low block will not be created
      */
     @Test
-    public void testCreateblock_6args() {
-        System.out.println("createblock");
-        double gradient = 0.0;
-        String beaconData = "";
-        int authority = 0;
-        int speedlimit = 0;
-        int length = 0;
-        boolean tunnel = false;
-        Block expResult = null;
-        Block result = Block.createblock(gradient, beaconData, authority, speedlimit, length, tunnel);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testtolowgradient() {
+        Block result = Block.createblock(-3, "easy", 1, 5, 10, true, true);
+        assertNull(result);
+    }
+    
+    /**
+     * Test of if gradient is just right a block will be created and hold correct values
+     */
+    @Test
+    public void testjustrightgradient() {
+        for(double i = -2; i < 3; i++)
+        {
+            System.out.println("Tested Grade: "+ i);
+            Block result = Block.createblock(i, "easy", 1, 5, 10, true, true);
+            assertEquals(result.getGradient(),i,0);
+        }
+    }
+    
+    /**
+     * Test of if gradient is too high block will not be created
+     */
+    @Test
+    public void testtohighgradient() {
+        Block result = Block.createblock(3, "easy", 1, 5, 10, true, true);
+        assertNull(result);
     }
 
     /**
-     * Test of createblock method, of class Block.
+     * Test of if beaconstring is too long block will not be created
      */
     @Test
-    public void testCreateblock_7args() {
-        System.out.println("createblock");
-        double gradient = 0.0;
-        String beaconData = "";
-        int authority = 0;
-        int speedlimit = 0;
-        int length = 0;
-        boolean tunnel = false;
-        boolean yard = false;
-        Block expResult = null;
-        Block result = Block.createblock(gradient, beaconData, authority, speedlimit, length, tunnel, yard);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testtoolongbeaconstring() {
+        Block result = Block.createblock(3, "extremelylongbeaconstringthatshouldmakethistestcasefail", 1, 5, 10, true, true);
+        assertNull(result);
     }
-
+    
     /**
-     * Test of getGradient method, of class Block.
+     * Test of if beaconstring is null block will not be created
      */
     @Test
-    public void testGetGradient() {
-        System.out.println("getGradient");
-        Block instance = null;
-        double expResult = 0.0;
-        double result = instance.getGradient();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testnullbeaconstring() {
+        Block result = Block.createblock(3, null, 1, 5, 10, true, true);
+        assertNull(result);
     }
-
-    /**
-     * Test of getBeacon method, of class Block.
+    
+     /**
+     * Test of if beaconstring is correct block will store the strin
      */
     @Test
-    public void testGetBeacon() {
-        System.out.println("getBeacon");
-        Block instance = null;
-        String expResult = "";
-        String result = instance.getBeacon();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testcorrectbeaconstring() {
+        String[] teststrings = {"test1" , "testtwo", "TestThreee"};
+        for(int i = 0; i < teststrings.length;i++)
+        {
+            System.out.println("Testing String: " + teststrings[i]);
+            Block result = Block.createblock(0, teststrings[i], 1, 5, 10, true, true);
+            assertTrue(result.getBeacon().equals(teststrings[i]));
+        }
     }
-
+    
     /**
-     * Test of getAuth method, of class Block.
+     * Test of if authority is negative block will not be created
      */
     @Test
-    public void testGetAuth() {
-        System.out.println("getAuth");
-        Block instance = null;
-        int expResult = 0;
-        int result = instance.getAuth();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testnegativeauthority() {
+        Block result = Block.createblock(0, "test", -1, 5, 10, true, true);
+        assertNull(result);
     }
-
+    
     /**
-     * Test of getSpeed method, of class Block.
+     * Test of if authority is positive block will be created
      */
     @Test
-    public void testGetSpeed() {
-        System.out.println("getSpeed");
-        Block instance = null;
-        int expResult = 0;
-        int result = instance.getSpeed();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testpositiveauthority() {
+        Block result = Block.createblock(0, "test", 1, 5, 10, true, true);
+        assertEquals(1,result.getAuth());
     }
-
+    
     /**
-     * Test of getLength method, of class Block.
+     * Test of if authority is zero block will be created
      */
     @Test
-    public void testGetLength() {
-        System.out.println("getLength");
-        Block instance = null;
-        int expResult = 0;
-        int result = instance.getLength();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testzeroauthority() {
+        Block result = Block.createblock(0, "test", 0, 5, 10, true, true);
+        assertEquals(0,result.getAuth());
     }
-
+    
     /**
-     * Test of isTunnel method, of class Block.
+     * Test of if speedlimit is negative block will not be created
      */
     @Test
-    public void testIsTunnel() {
-        System.out.println("isTunnel");
-        Block instance = null;
-        boolean expResult = false;
-        boolean result = instance.isTunnel();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testnegativespeedlimit() {
+        Block result = Block.createblock(0, "test", 1, -1, 10, true, true);
+        assertNull(result);
     }
-
+    
     /**
-     * Test of isYard method, of class Block.
+     * Test of if speedlimit is positive block will be created
      */
     @Test
-    public void testIsYard() {
-        System.out.println("isYard");
-        Block instance = null;
-        boolean expResult = false;
-        boolean result = instance.isYard();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testpositivespeedlimit() {
+        Block result = Block.createblock(0, "test", 1, 1, 10, true, true);
+        assertEquals(1,result.getSpeed(),0);
+    }
+    
+    /**
+     * Test of if speedlimit is zero block will be created
+     */
+    @Test
+    public void testzerospeedlimit() {
+        Block result = Block.createblock(0, "test", 1, 0, 10, true, true);
+        assertEquals(0,result.getSpeed(),0);
+    }
+    
+    /**
+     * Test of if length is negative block will not be created
+     */
+    @Test
+    public void testnegativelength() {
+        Block result = Block.createblock(0, "test", 1, 1, -10, true, true);
+        assertNull(result);
+    }
+    
+    /**
+     * Test of if length is positive block will be created
+     */
+    @Test
+    public void testpositivelength() {
+        Block result = Block.createblock(0, "test", 1, 1, 10, true, true);
+        assertEquals(1,result.getLength(),10);
+    }
+    
+    /**
+     * Test of if length is zero block will not be created
+     */
+    @Test
+    public void testzerolength() {
+        Block result = Block.createblock(0, "test", 1, 1, 0, true, true);
+         assertNull(result);
+    }
+    
+    /**
+     * Test block creation stores tunnel correctly
+     */
+    @Test
+    public void testtunnel() {
+        Block result = Block.createblock(0, "test", 1, 1, 10, true, false);
+        assertTrue(result.isTunnel());
+        result = Block.createblock(0, "test", 1, 1, 10, false, true);
+        assertFalse(result.isTunnel());
+    }
+    
+    /**
+     * Test block creation stores yard correctly
+     */
+    @Test
+    public void testyard() {
+        Block result = Block.createblock(0, "test", 1, 1, 10, false, true);
+        assertTrue(result.isYard());
+        result = Block.createblock(0, "test", 1, 1, 10, true, false);
+        assertFalse(result.isYard());
+    }
+    
+    /**
+     * Test 5 arg block creation defaults yard to false
+     */
+    @Test
+    public void test5arg() {
+        Block result = Block.createblock(0, "test", 1, 1, 10, true);
+        assertFalse(result.isYard());
     }
     
 }
