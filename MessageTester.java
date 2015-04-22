20d6ecd0667ae8d706a806822c79585cb6d38e8a
modishalin <modishalin@gmail.com>
2015-04-22 19:45:17 -0400

    Create MessageTester.java

---
+++ MessageTester.java
@@ -0,0 +1,18 @@
+package src;
+import java.io.*;
+
+	public class MessageTester {
+		
+		public static void main(String args[]) {
+			String message;
+			while (true) {
+				System.out.println("Enter a message: ");
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				try {
+					message = br.readLine();
+					MessageLibrary.sendMessage(8003, message);
+				}
+				catch (Exception e) {}				
+			}
+		}
+}
