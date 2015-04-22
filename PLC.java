821d7b53442f89ff8e373fc6f026e3c3e0cc6b5c
modishalin <modishalin@gmail.com>
2015-04-22 19:41:51 -0400

    Create PLC.java

---
+++ PLC.java
@@ -0,0 +1,40 @@
+//Shalin Modi
+//Track Controller PLC
+//Prototype
+
+package src;
+
+import java.util.*;
+
+public class PLC {
+	//Fields logic
+	public PLC() {
+	}
+	
+	public boolean verifyPLC() {
+		return true;
+	}
+	
+	public void switchToggle() {}
+	
+	public void switchLights() {
+		
+	}
+	
+	public void railCrossingBar() {
+		//if track occupancy on the crossing block true then rail bar down
+	}
+	
+	/*public boolean checkTrack (Hashtable<Integer, Block> oldState, Hashtable<Integer, Block> newState, Block newBlock) {
+		return !oldState.get(newBlock.getID()).isOccupiedNoMaintenance() && newState.get(newBlock.getID()0.isOccupiedNoMaintenance();
+	}
+	
+	public void checkCrossing (Block occupied) {
+		if (occupied.isCrossing() && occupied.isOccupied()) {
+			occupied.setCrossing(true);
+		}
+		else {
+			occupied.setCrossing(false);
+		}
+	}*/
+}
