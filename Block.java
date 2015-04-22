2fc1c7c9fed7dd6f48ca28e2bd140ecfc0b5bf43
modishalin <modishalin@gmail.com>
2015-04-22 19:41:22 -0400

    Create Block.java

---
+++ Block.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.*;
+
+public class Block
+{
+    public boolean trainPresent() { //track occupancy
+		return false;
+    }
+    
+    public boolean brokenRail(String message) { //maintenance, crashing
+    	MessageLibrary.sendMessage(8001, "Track Controller : Green 1 : set, Status = Closed");
+    	MessageLibrary.sendMessage(8001, "Track Controller : Green 1 : set, Lights = Red");
+    	//tcGUI.uiElements.get("lights").setText("R");
+    	return true;
+    }
+    
+    /*public boolean trcHeater() { //weather
+    	//if (weather is bad)
+    		//MessageLibrary.sendMessage(8001, "Track Controller : Red|Green 0 : set, Heater = On");
+    	return false;
+    }*/
+}
