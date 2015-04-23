3340a67ee21096e8d1fd1ce757393016938c028a
modishalin <modishalin@gmail.com>
2015-04-23 17:10:30 -0400

    Update PLC.java

--- PLC.java
+++ PLC.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 public class PLC {
 	//Fields logic
+	
+	//This goes through the path and decides if it is a switch block and if it is, decides if
+	//the switch needs to be toggled or not based on the next block
 	public boolean switchToggle(String message) {
 		String[] messageParts = message.replaceAll("\\s+","").toLowerCase().split(":");
 		ArrayList<Integer> stopList = new ArrayList<Integer>();
@@ -19,12 +22,13 @@ public class PLC {
 				String subjectID = messageParts[1].replaceAll("[^0-9]", ""); // The ID of the subject.  This is an integer.
 				String JSONMessage = "{'commandType':'" + commandType + "', 'commandValue':'" + commandValue + "', 'subjectType':'" + subjectType + "', 'subjectID':" + subjectID + "}";
 				JSONMessage = JSONMessage.replaceAll("'", "\"");
-				
 				if(commandType.equals("path")) {
 					String[] route = setParts[1].replace("[", "").replace("]", "").split(","); 
 					for (int i = 0; i < route.length; i++) {
+						for(int j = 1; j < route.length+1; j++) {
 						route[i].trim(); 
-						if (route[i].contains("{")) {
+						//System.out.println("in for");
+						if (route[i].charAt(0) == '{') {
 							String [] blockDwell = route[i].split("/");
 							String stopString = blockDwell[0].replaceAll("[^0-9]", "");
 							stopList.add(Integer.parseInt(stopString));
@@ -33,87 +37,88 @@ public class PLC {
 							dwellList.add(Integer.parseInt(dwellString));
 							//System.out.println(dwellString);
 						}
+						
 						if (subjectType.equals("red")) {
-							if (route[i].equals("1") && route[i+1].equals("15")) {
-								tcGUI.uiElements.get("switchNum").setText("1, 15");
+							if (route[i].equals("1") && route[j].equals("15")) {
+								tcGUI.uiElements.get("switchnum").setText("1, 15");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [1, 15]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [1, 15]");
 								return true;
 							}
-							else if (route[i].equals("1") && route[i+1].equals("16")) {
-								tcGUI.uiElements.get("switchNum").setText("1, 16");
+							else if (route[i].equals("1") && route[j].equals("16")) {
+								tcGUI.uiElements.get("switchnum").setText("1, 16");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [1, 16]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [1, 16]");
 								return true;
 							}
-							if (route[i].equals("27") && route[i+1].equals("28")) {
-								tcGUI.uiElements.get("switchNum").setText("27, 28");
+							if (route[i].equals("27") && route[j].equals("28")) {
+								tcGUI.uiElements.get("switchnum").setText("27, 28");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [27, 28]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [27, 28]");
 								return true;
 							}
-							else if (route[i].equals("27") && route[i+1].equals("76")) {
-								tcGUI.uiElements.get("switchNum").setText("27, 76");
+							else if (route[i].equals("27") && route[j].equals("76")) {
+								tcGUI.uiElements.get("switchnum").setText("27, 76");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [27, 76]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [27, 76]");
 								return true;
 							}
-							if (route[i].equals("32") && route[i+1].equals("33")) {
-								tcGUI.uiElements.get("switchNum").setText("32, 33");
+							if (route[i].equals("32") && route[j].equals("33")) {
+								tcGUI.uiElements.get("switchnum").setText("32, 33");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [32, 33]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [32, 33]");
 								return true;
 							}
-							else if (route[i].equals("32") && route[i+1].equals("72")) {
-								tcGUI.uiElements.get("switchNum").setText("32, 72");
+							else if (route[i].equals("32") && route[j].equals("72")) {
+								tcGUI.uiElements.get("switchnum").setText("32, 72");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [32, 72]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [32, 72]");
 								return true;
 							}
-							if (route[i].equals("38") && route[i+1].equals("39")) {
-								tcGUI.uiElements.get("switchNum").setText("38, 39");
+							if (route[i].equals("38") && route[j].equals("39")) {
+								tcGUI.uiElements.get("switchnum").setText("38, 39");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [38, 39]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [38, 39]");
 								return true;
 							}
-							else if (route[i].equals("38") && route[i+1].equals("71")) {
-								tcGUI.uiElements.get("switchNum").setText("38, 71");
+							else if (route[i].equals("38") && route[j].equals("71")) {
+								tcGUI.uiElements.get("switchnum").setText("38, 71");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [38, 71]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [38, 71]");
 								return true;
 							}
-							if (route[i].equals("43") && route[i+1].equals("44")) {
-								tcGUI.uiElements.get("switchNum").setText("43, 44");
+							if (route[i].equals("43") && route[j].equals("44")) {
+								tcGUI.uiElements.get("switchnum").setText("43, 44");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [43, 44]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [43, 44]");
 								return true;
 							}
-							else if (route[i].equals("43") && route[i+1].equals("67")) {
-								tcGUI.uiElements.get("switchNum").setText("43, 67");
+							else if (route[i].equals("43") && route[j].equals("67")) {
+								tcGUI.uiElements.get("switchnum").setText("43, 67");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [43, 67]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [43, 67]");
 								return true;
 							}
-							if (route[i].equals("52") && route[i+1].equals("53")) {
-								tcGUI.uiElements.get("switchNum").setText("52, 53");
+							if (route[i].equals("52") && route[j].equals("53")) {
+								tcGUI.uiElements.get("switchnum").setText("52, 53");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [52, 53]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [52, 53]");
 								return true;
 							}
-							else if (route[i].equals("52") && route[i+1].equals("66")) {
-								tcGUI.uiElements.get("switchNum").setText("52, 66");
+							else if (route[i].equals("52") && route[j].equals("66")) {
+								tcGUI.uiElements.get("switchnum").setText("52, 66");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [52, 66]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [52, 66]");
 								return true;
 							}
-							if (route[i].equals("9") && route[i+1].equals("10")) {
-								tcGUI.uiElements.get("switchNum").setText("9, 10");
+							if (route[i].equals("9") && route[j].equals("10")) {
+								tcGUI.uiElements.get("switchnum").setText("9, 10");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [9, 10]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [9, 10]");
 								return true;
 							}
-							else if (route[i].equals("9") && route[i+1].equals("77")) {
-								tcGUI.uiElements.get("switchNum").setText("9, 77");
+							else if (route[i].equals("9") && route[j].equals("77")) {
+								tcGUI.uiElements.get("switchnum").setText("9, 77");
 								MessageLibrary.sendMessage(8001, "Track Controller : Red "+subjectID+" : set, Switch = [9, 77]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Red "+subjectID+" : set, Switch = [9, 77]");
 								return true;
@@ -121,78 +126,80 @@ public class PLC {
 							
 						}
 						else if (subjectType.equals("green")) {
-							if (route[i].equals("61") && route[i+1].equals("62")) {
-								tcGUI.uiElements.get("switchNum").setText("61, 62");
+							if (route[i].equals("61") && route[j].equals("62")) {
+								tcGUI.uiElements.get("switchnum").setText("61, 62");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [61, 62]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [61, 62]");
 								return true;
 							}
-							else if (route[i].equals("61") && route[i+1].equals("152")) {
-								tcGUI.uiElements.get("switchNum").setText("61, 152");
+							else if (route[i].equals("61") && route[j].equals("152")) {
+								tcGUI.uiElements.get("switchnum").setText("61, 152");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [61, 152]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [61, 152]");
 								return true;
 							}
-							if (route[i].equals("1") && route[i+1].equals("12")) {
-								tcGUI.uiElements.get("switchNum").setText("1, 12");
+							if (route[i].equals("1") && route[j].equals("12")) {
+								System.out.println("in switch block");
+								tcGUI.uiElements.get("switchnum").setText("1, 12");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [1, 12]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [1, 12]");
 								return true;
 							}
-							else if (route[i].equals("1") && route[i+1].equals("13")) {
-								tcGUI.uiElements.get("switchNum").setText("1, 13");
+							else if (route[i].equals("1") && route[j].equals("13")) {
+								tcGUI.uiElements.get("switchnum").setText("1, 13");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [1, 13]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [1, 13]");
 								return true;
 							}
-							if (route[i].equals("28") && route[i+1].equals("29")) {
-								tcGUI.uiElements.get("switchNum").setText("28, 29");
+							if (route[i].equals("28") && route[j].equals("29")) {
+								tcGUI.uiElements.get("switchnum").setText("28, 29");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [28, 29]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [28, 29]");
 								return true;
 							}
-							else if (route[i].equals("28") && route[i+1].equals("150")) {
-								tcGUI.uiElements.get("switchNum").setText("28, 150");
+							else if (route[i].equals("28") && route[j].equals("150")) {
+								tcGUI.uiElements.get("switchnum").setText("28, 150");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [28, 150]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [28, 150]");
 								return true;
 							}
-							if (route[i].equals("57") && route[i+1].equals("58")) {
-								tcGUI.uiElements.get("switchNum").setText("57, 58");
+							if (route[i].equals("57") && route[j].equals("58")) {
+								tcGUI.uiElements.get("switchnum").setText("57, 58");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [57, 58]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [57, 58]");
 								return true;
 							}
-							else if (route[i].equals("57") && route[i+1].equals("151")) {
-								tcGUI.uiElements.get("switchNum").setText("57, 151");
+							else if (route[i].equals("57") && route[j].equals("151")) {
+								tcGUI.uiElements.get("switchnum").setText("57, 151");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [57, 151]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [57, 151]");
 								return true;
 							}
-							if (route[i].equals("76") && route[i+1].equals("77")) {
-								tcGUI.uiElements.get("switchNum").setText("76, 77");
+							if (route[i].equals("76") && route[j].equals("77")) {
+								tcGUI.uiElements.get("switchnum").setText("76, 77");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [76, 77]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [76, 77]");
 								return true;
 							}
-							else if (route[i].equals("76") && route[i+1].equals("101")) {
-								tcGUI.uiElements.get("switchNum").setText("76, 101");
+							else if (route[i].equals("76") && route[j].equals("101")) {
+								tcGUI.uiElements.get("switchnum").setText("76, 101");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [76, 101]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [76, 101]");
 								return true;
 							}
-							if (route[i].equals("85") && route[i+1].equals("86")) {
-								tcGUI.uiElements.get("switchNum").setText("85, 86");
+							if (route[i].equals("85") && route[j].equals("86")) {
+								tcGUI.uiElements.get("switchnum").setText("85, 86");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [85, 86]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [85, 86]");
 								return true;
 							}
-							else if (route[i].equals("85") && route[i+1].equals("100")) {
-								tcGUI.uiElements.get("switchNum").setText("85, 100");
+							else if (route[i].equals("85") && route[j].equals("100")) {
+								tcGUI.uiElements.get("switchnum").setText("85, 100");
 								MessageLibrary.sendMessage(8001, "Track Controller : Green "+subjectID+" : set, Switch = [85, 100]");
 								MessageLibrary.sendMessage(8005, "Track Controller : Green "+subjectID+" : set, Switch = [85, 100]");
 								return true;
 							}
+						}
 						}
 					}
 				}
@@ -249,7 +256,7 @@ public class PLC {
 					String[] route = setParts[1].replace("[", "").replace("]", "").split(","); 
 					for (int i = 0; i < route.length; i++) {
 						route[i].trim(); 
-						if (route[i].contains("{")) {
+						if (route[i].charAt(0) == '{') {
 							String [] blockDwell = route[i].split("/");
 							String stopString = blockDwell[0].replaceAll("[^0-9]", "");
 							stopList.add(Integer.parseInt(stopString));
